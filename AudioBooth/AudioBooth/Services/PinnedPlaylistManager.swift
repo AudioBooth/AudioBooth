c1a45944d29ae20fb504eba0d8ecb81fd37bce60
Jeremy Grenier <1220338+jeremygrenier@users.noreply.github.com>
2026-02-14 10:54:31 -0800

    Improve pin playlist to be by library

--- AudioBooth/AudioBooth/Services/PinnedPlaylistManager.swift
+++ AudioBooth/AudioBooth/Services/PinnedPlaylistManager.swift
@@ -1,3 +1,4 @@
+import API
 import Combine
 import Foundation
 import SwiftUI
@@ -5,12 +6,29 @@ import SwiftUI
 final class PinnedPlaylistManager: ObservableObject {
   static let shared = PinnedPlaylistManager()
 
-  @AppStorage("pinnedPlaylistID")
-  var pinnedPlaylistID: String?
-
+  private let userDefaults = UserDefaults.standard
   private let preferences = UserPreferences.shared
 
-  private init() {}
+  private init() {
+    migrateIfNeeded()
+  }
+
+  var pinnedPlaylistID: String? {
+    get {
+      guard let key else { return nil }
+      return userDefaults.string(forKey: key)
+    }
+    set {
+      guard let key else { return }
+      objectWillChange.send()
+
+      if let newValue {
+        userDefaults.set(newValue, forKey: key)
+      } else {
+        userDefaults.removeObject(forKey: key)
+      }
+    }
+  }
 
   func pin(_ playlistID: String) {
     pinnedPlaylistID = playlistID
@@ -22,10 +40,23 @@ final class PinnedPlaylistManager: ObservableObject {
 
   func unpin() {
     pinnedPlaylistID = nil
-    preferences.homeSections.removeAll { $0 == .pinnedPlaylist }
   }
 
   func isPinned(_ playlistID: String) -> Bool {
     pinnedPlaylistID == playlistID
+  }
+
+  private var key: String? {
+    guard let libraryID = Audiobookshelf.shared.libraries.current?.id else { return nil }
+    return "pinnedPlaylistID_\(libraryID)"
+  }
+
+  private func migrateIfNeeded() {
+    let legacyKey = "pinnedPlaylistID"
+    guard let legacyValue = userDefaults.string(forKey: legacyKey) else { return }
+    guard let key else { return }
+
+    userDefaults.set(legacyValue, forKey: key)
+    userDefaults.removeObject(forKey: legacyKey)
   }
 }
