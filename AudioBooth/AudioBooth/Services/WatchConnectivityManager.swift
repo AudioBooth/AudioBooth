ee7f1a6c7c7d21b0678bd5cbe02f8e523ae7a6e6
Jeremy Grenier <1220338+jeremygrenier@users.noreply.github.com>
2026-09-13 20:23:31 -0700

    Fix iOS 27 issues

--- AudioBooth/AudioBooth/Services/WatchConnectivityManager.swift
+++ AudioBooth/AudioBooth/Services/WatchConnectivityManager.swift
@@ -241,7 +241,7 @@ extension WatchConnectivityManager: WCSessionDelegate {
 
       Task {
         if activationState == .activated, Audiobookshelf.shared.authentication.server != nil {
-          try await Task.sleep(nanoseconds: 1_000_000_000)
+          try? await Task.sleep(nanoseconds: 1_000_000_000)
           syncCachedDataToWatch()
         }
       }
